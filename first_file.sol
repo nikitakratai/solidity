bd69f8f13e055d38587ee8618927c39edf85db54
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-03-04 13:20:17 +0700

    Create first_file.sol

---
+++ first_file.sol
@@ -0,0 +1,17 @@
+contract Contract1
+{
+  mapping(address=>uint) balances;
+  uint constant totalCoins = 1000000;
+
+  function send(address to, uint256 valueInmGAV) {
+    if (balances[msg.sender] >= valueInmGAV) {
+      balances[to] += valueInmGAV;
+      balances[msg.sender] -= valueInmGAV;
+    }
+  }
+
+  function balance(address who) constant returns (uint256 balanceInmGAV) {
+    balanceInmGAV = balances[who];
+  }
+
+};
